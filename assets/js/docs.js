e9e12f7c15b59a465bd2627b3e01a37e3cecdc84
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-09 23:04:05 +0530

    docs.js - cleaned

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,14 +1,9 @@
 "use strict";
 
-
-/* ====== Define JS Constants ====== */
 const sidebarToggler = document.getElementById('docs-sidebar-toggler');
 const sidebar = document.getElementById('docs-sidebar');
 const sidebarLinks = document.querySelectorAll('#docs-sidebar .scrollto');
 
-
-
-/* ===== Responsive Sidebar ====== */
 
 window.onload=function() 
 { 
@@ -24,13 +19,11 @@ window.onresize=function()
 function responsiveSidebar() {
     let w = window.innerWidth;
 	if(w >= 1200) {
-	    // if larger 
 	    console.log('larger');
 		sidebar.classList.remove('sidebar-hidden');
 		sidebar.classList.add('sidebar-visible');
 		
 	} else {
-	    // if smaller
 	    console.log('smaller');
 	    sidebar.classList.remove('sidebar-visible');
 		sidebar.classList.add('sidebar-hidden');
@@ -51,10 +44,6 @@ sidebarToggler.addEventListener('click', () => {
 });
 
 
-/* ===== Smooth scrolling ====== */
-/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
-/* Ref: https://github.com/iamdustan/smoothscroll */
-
 sidebarLinks.forEach((sidebarLink) => {
 	
 	sidebarLink.addEventListener('click', (e) => {
@@ -63,12 +52,10 @@ sidebarLinks.forEach((sidebarLink) => {
 		
 		var target = sidebarLink.getAttribute("href").replace('#', '');
 		
-		//console.log(target);
 		
         document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
         
         
-        //Collapse sidebar after clicking
 		if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200){
 			
 			sidebar.classList.remove('sidebar-visible');
@@ -79,27 +66,7 @@ sidebarLinks.forEach((sidebarLink) => {
 	
 });
 
-
-/* ===== Gumshoe SrollSpy ===== */
-/* Ref: https://github.com/cferdinandi/gumshoe  */
-// Initialize Gumshoe
 var spy = new Gumshoe('#docs-nav a', {
-	offset: 69, //sticky header height
+	offset: 69, 
 });
-
-
-/* ====== SimpleLightbox Plugin ======= */
-/*  Ref: https://github.com/andreknieriem/simplelightbox */
-
 var lightbox = new SimpleLightbox('.simplelightbox-gallery a', {/* options */});
-
-
-
-
-
-
-
-
-
-
-
